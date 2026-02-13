f8cd24cdcb5ae4ee15373dda3ceab456c66687ec
sergioGyarab <serhii.khudanych.s@gyarab.cz>
2026-02-13 09:58:50 +0100

    changed settings

--- prj/prj/settings.py
+++ prj/prj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yh4c&01(-v(vw7pr*8y@k@79*lsjbx$mreccjh+x#w1n3^e^rb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
