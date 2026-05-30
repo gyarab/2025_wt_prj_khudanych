2d665727dcd031acd83a3be46f06c01231c87aea
sergioGyarab <serhii.khudanych.s@gyarab.cz>
2026-05-30 17:00:30 +0200

    Add CountryFlag model, views, and templates; update .gitignore

--- prj/app/admin.py
+++ prj/app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import CountryFlag
 
 # Register your models here.
+admin.site.register(CountryFlag)
--- prj/app/views.py
+++ prj/app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, HttpResponse
 
+from .models import CountryFlag
+
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def flags(request):
+    flags = CountryFlag.objects.all()
+    return render(request, "flags.html", {"flags": flags})
